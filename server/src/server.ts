e7c434d8395fb978dbac942fa10c487a0e253c62
rgoliveira <rgoliveira1441973@gmail.com>
2023-05-20 16:51:02 -0300

    feat(server): Implement upload feature

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,8 +4,18 @@ import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -16,6 +26,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
