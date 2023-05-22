347cafde59e35ffcce0ff2f62f70ada2614882de
rgoliveira <rgoliveira1441973@gmail.com>
2023-05-21 22:28:17 -0300

    feat(web): Enable upload via new memory form

--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -1,4 +1,5 @@
-import { Camera, ChevronLeft } from 'lucide-react'
+import { NewMemoryForm } from '@/components/NewMemoryForm'
+import { ChevronLeft } from 'lucide-react'
 import Link from 'next/link'
 
 export default function NewMemory() {
@@ -12,39 +13,7 @@ export default function NewMemory() {
         voltar à timeline
       </Link>
 
-      <form className="flex flex-1 flex-col gap-2" action="">
-        <div className="flex items-center gap-4">
-          <label
-            className="hover: flex cursor-pointer items-center gap-1.5 text-sm text-gray-100 text-gray-200"
-            htmlFor="media"
-          >
-            <Camera className="h-4 w-4" />
-            Anexar mídia
-          </label>
-
-          <label
-            htmlFor="isPublic"
-            className="flex items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
-          >
-            <input
-              type="checkbox"
-              name="isPublic"
-              id="isPublic"
-              value="true"
-              className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500"
-            />
-            Tornar memória pública
-          </label>
-        </div>
-        <input type="file" id="media" className="invisible h-0 w-0" />
-
-        <textarea
-          name="content"
-          spellCheck={false}
-          className="placeholder:text-grey-400 w-full flex-1 resize-none border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 focus:ring-0"
-          placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experência que você quer lembrar para sempre."
-        />
-      </form>
+      <NewMemoryForm />
     </div>
   )
 }
